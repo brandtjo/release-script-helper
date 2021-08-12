84e176c12bb6e2b87b4e70b252cc39472347f203
GitHub Action <action@github.com>
2021-08-12 18:34:44 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/brandtjo/releasescripthelper/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.brandtjo.releasescripthelper.services
+
+import com.github.brandtjo.releasescripthelper.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
